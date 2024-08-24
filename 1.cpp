5aa80476059e3690893def05fae6b716b1cc24cb
ibrahimwarraich101 <ibrahimwarraich09@gmail.com>
2024-08-24 14:58:32 +0500

    Updated

--- 1.cpp
+++ 1.cpp
@@ -7,17 +7,13 @@ class Solution
 public:
 	bool isPalindrome(string s)
 	{
-		remove(s);
-		for (int i = 0; i < remove(s).length(); i++)
+		s = remove(s);
+		for (int i = 0; i < s.length(); i++)
 		{
-			for (int i = 0; i < remove(s).length() / 2; i++)
-			{
-				if (remove(s)[i] != remove(s)[remove(s).length() - i - 1])
-					return false;
-				else
-					return true;
-			}
+			if (s[i] != s[s.length() - i - 1])
+				return false;
 		}
+		return true;
 	}
 
 	string remove(string sen)
@@ -25,12 +21,13 @@ public:
 		string result;
 		for (int i = 0; i < sen.length(); i++)
 		{
-			if (sen[i] >= 48 && sen[i] <= 57 || sen[i] >= 32 && sen[i] <= 47)
-				continue;
-			else if (sen[i] >= 65 && sen[i] <= 90)
-				result += (sen[i] + 32);
-			else
-				result += sen[i];
+			if ((sen[i] >= 48 && sen[i] <= 57) || (sen[i] >= 65 && sen[i] <= 90) || (sen[i] >= 97 && sen[i] <= 122))
+			{
+				if (sen[i] >= 65 && sen[i] <= 90)
+					result += (sen[i] + 32);
+				else
+					result += sen[i];
+			}
 		}
 		return result;
 	}
@@ -40,7 +37,9 @@ int main()
 {
 	Solution sol;
 	string sen;
-	cout << "Enter a string to check it's a palindrome or not: "; getline(cin, sen); cout << endl;
+	cout << "Enter a string to check if it's a palindrome or not: ";
+	getline(cin, sen);
+	cout << endl;
 
 	if (sol.isPalindrome(sen))
 		cout << "String is Palindrome.\n";
